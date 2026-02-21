fbd0f777ad5342e21f41965148f3fe22b0bfd902
daniel <daniel.iwanicki@armlab.ai>
2026-02-21 12:33:20 +0100

    Fixes

--- lib/src/pdfwidgets/quote_widget.dart
+++ lib/src/pdfwidgets/quote_widget.dart
@@ -3,33 +3,37 @@ import '../utils/app_assets.dart';
 
 Widget buildQuoteWidget(Widget child, {required HtmlTagStyle customStyles}) {
   final quoteColor = customStyles.quoteBarColor ?? PdfColors.grey600;
-  return Container(
-    margin: const EdgeInsets.symmetric(vertical: 8),
-    padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-    decoration: BoxDecoration(
-      color: PdfColors.grey100,
-      borderRadius: BorderRadius.all(Radius.circular(customStyles.borderRadius)),
-      border: Border(
-        left: BorderSide(
-          color: quoteColor,
-          width: 3,
-        ),
-      ),
-    ),
-    child: Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 6, top: 2),
-          child: SvgImage(
-            svg: AppAssets.quoteIcon,
-            width: 16,
-            height: 16,
-            colorFilter: quoteColor,
+  final radius = Radius.circular(customStyles.borderRadius);
+  return ClipRRect(
+    horizontalRadius: customStyles.borderRadius,
+    verticalRadius: customStyles.borderRadius,
+    child: Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+      decoration: BoxDecoration(
+        color: PdfColors.grey100,
+        border: Border(
+          left: BorderSide(
+            color: quoteColor,
+            width: 3,
           ),
         ),
-        Expanded(child: child),
-      ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 6, top: 2),
+            child: SvgImage(
+              svg: AppAssets.quoteIcon,
+              width: 16,
+              height: 16,
+              colorFilter: quoteColor,
+            ),
+          ),
+          Expanded(child: child),
+        ],
+      ),
     ),
   );
 }
