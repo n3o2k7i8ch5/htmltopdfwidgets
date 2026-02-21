36ea69414aea7295c3a5f2590ec3e3424d6c2d78
daniel <daniel.iwanicki@armlab.ai>
2026-02-21 12:06:22 +0100

    Fixes

--- lib/src/pdfwidgets/quote_widget.dart
+++ lib/src/pdfwidgets/quote_widget.dart
@@ -1,6 +1,8 @@
 import '../../html_pdf_widgets.dart';
+import '../utils/app_assets.dart';
 
 Widget buildQuoteWidget(Widget child, {required HtmlTagStyle customStyles}) {
+  final quoteColor = customStyles.quoteBarColor ?? PdfColors.grey600;
   return Container(
     margin: const EdgeInsets.symmetric(vertical: 8),
     padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
@@ -8,11 +10,25 @@ Widget buildQuoteWidget(Widget child, {required HtmlTagStyle customStyles}) {
       color: PdfColors.grey100,
       border: Border(
         left: BorderSide(
-          color: customStyles.quoteBarColor ?? PdfColors.grey600,
+          color: quoteColor,
           width: 3,
         ),
       ),
     ),
-    child: child,
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 6, top: 2),
+          child: SvgImage(
+            svg: AppAssets.quoteIcon,
+            width: 16,
+            height: 16,
+            colorFilter: quoteColor,
+          ),
+        ),
+        Expanded(child: child),
+      ],
+    ),
   );
 }
